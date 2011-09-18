ce28d3373b392e517843a7fa4a77ecc510202129
Dan Bettles <dan@danbettles.net>
2011-09-18 11:34:04 +0100

    Reduced the coupling between the toaster and the alert

--- include/jquery.alerttoaster.js
+++ include/jquery.alerttoaster.js
@@ -37,144 +37,209 @@ jQuery.extend(true, {
         /**
          * Creates the Alert object
          * 
+         * Only one alert is ever needed so only one is ever created
+         * 
          * @private
+         * @param {Object} [p_oOptions]
          * @return Alert
          */
-        createAlert: function () {
-            var oAlertToaster = this,
-                //@todo Update this so it's the alert only - it's the alert, alone, that's created only the once
-                //@todo Z-index should be controlled by the toaster, probably
-                oAlert = (function () {
+        createAlert: function (p_oOptions) {
+            var oOptions = jQuery.extend({
+                buttonLabel: 'OK',
+                baseZIndex: 9999
+            }, jQuery.isPlainObject(p_oOptions) ? p_oOptions : {});
+
+            return (function (p_oOptions) {
+                var oAlert = this;
+
+                /**
+                 * @private
+                 * @type Object
+                 */
+                this.oOptions = p_oOptions;
+
+                /**
+                 * @private
+                 * @type jQuery
+                 */
+                this.oButtonEl = null;
+
+                /**
+                 * @private
+                 * @type jQuery
+                 */
+                this.oButtonWrapperEl = null;
+
+                /**
+                 * @private
+                 * @type jQuery
+                 */
+                this.oMessageEl = null;
+
+                /**
+                 * @private
+                 * @type jQuery
+                 */
+                this.oAlertEl = null;
+
+                /**
+                 * @private
+                 * @type jQuery
+                 */
+                this.oBackgroundEl = null;
+
+                /**
+                 * Returns the specified option, or all options if a name is not passed
+                 * 
+                 * @param {String} [p_name]
+                 * @return Mixed
+                 * @throws "The option does not exist" if the requested option does not exist
+                 */
+                this.getOptions = function (p_name) {
+                    if (typeof p_name === 'string') {
+                        if (typeof this.oOptions[p_name] === 'undefined') {
+                            throw 'The option does not exist';
+                        }
+
+                        return this.oOptions[p_name];
+                    }
+
+                    return this.oOptions;
+                };
+
+                /**
+                 * Sets the message
+                 * 
+                 * @private
+                 * @param {String} p_message
+                 */
+                this.setMessage = function (p_message) {
+                    this.oMessageEl.html(p_message);
+                };
+
+                /**
+                 * Clears the message
+                 * 
+                 * @private
+                 */
+                this.clearMessage = function () {
+                    this.setMessage('');
+                };
+
+                /**
+                 * Sets the position of the alert
+                 *
+                 * @param {Number} p_left
+                 * @param {Number} p_top
+                 */
+                this.setPosition = function (p_left, p_top) {
+                    this.oAlertEl.css({ left: p_left, top: p_top });
+                };
+
+                /**
+                 * Returns the dimensions of the alert
+                 *
+                 * @return Object
+                 */
+                this.getDimensions = function () {
                     var oAlert = this;
 
-                    /**
-                     * @private
-                     * @type {Integer}
-                     */
-                    this.baseZIndex = 9999;
+                    return {
+                        width: oAlert.oAlertEl.width(),
+                        height: oAlert.oAlertEl.height()
+                    };
+                };
 
-                    /**
-                     * @private
-                     * @type {jQuery}
-                     */
-                    this.oButtonEl = jQuery('<a href="#"><span>OK</span></a>')
-                        .click(function () {
+                /**
+                 * Displays the specified message
+                 * 
+                 * @param {String} p_message
+                 */
+                this.show = function (p_message) {
+                    var oAlert = this;
+
+                    this.setMessage(p_message);
+
+                    this.oBackgroundEl.fadeIn('fast', function () {
+                        oAlert.oAlertEl.slideDown('fast');
+                        oAlert.oButtonEl.focus();
+                    });
+                };
+
+                /**
+                 * Hides the alert
+                 */
+                this.hide = function () {
+                    var oAlert = this;
+
+                    this.oAlertEl.slideUp('fast', function () {
+                        oAlert.oBackgroundEl.fadeOut('fast');
+                        oAlert.clearMessage();
+                    });
+                };
+
+                this.oButtonEl = jQuery('<a href="#"/>')
+                    .append(jQuery('<span/>').html(this.getOptions('buttonLabel')))
+                    .click(function () {
+                        oAlert.hide();
+                        return false;
+                    });
+
+                this.oButtonWrapperEl = jQuery('<p id="alerttoaster-button" />')
+                    .append(this.oButtonEl);
+
+                this.oMessageEl = jQuery('<p id="alerttoaster-message" />');
+
+                this.oAlertEl = jQuery('<div id="alerttoaster-alert" />')
+                    .css({
+                        position: 'absolute',
+                        top: 0,
+                        zIndex: oAlert.getOptions('baseZIndex') + 1,
+                        display: 'none'
+                    })
+                    .click(function () {
+                        //Makes sure the alert is focused so the escape-key handler works
+                        oAlert.oButtonEl.focus();
+                    })
+                    .keydown(function (p_oEvent) {
+                        if (p_oEvent.keyCode === 27) {
                             oAlert.hide();
-                            return false;
-                        });
+                        }
+                    })
+                    .append(this.oMessageEl)
+                    .append(this.oButtonWrapperEl)
+                    .appendTo('body');
 
-                    /**
-                     * @private
-                     * @type {jQuery}
-                     */
-                    this.oButtonContainerEl = jQuery('<p id="alerttoaster-button" />')
-                        .append(this.oButtonEl);
+                this.oBackgroundEl = jQuery('<div id="alerttoaster-background" />')
+                    .css({
+                        position: 'absolute',
+                        left: 0,
+                        top: 0,
+                        zIndex: oAlert.getOptions('baseZIndex'),
+                        display: 'none',
+                        width: '100%',
+                        height: '100%'
+                    })
+                    .click(function () {
+                        //Makes sure the alert is focused so the escape-key handler works
+                        oAlert.oButtonEl.focus();
+                    })
+                    .appendTo('body');
 
-                    /**
-                     * @private
-                     * @type {jQuery}
-                     */
-                    this.oMessageEl = jQuery('<p id="alerttoaster-message" />');
+                return this;
+            }(oOptions));
+        },
 
-                    /**
-                     * @private
-                     * @type {jQuery}
-                     */
-                    this.oAlertEl = jQuery('<div id="alerttoaster-alert" />')
-                        .css({
-                            position: 'absolute',
-                            top: 0,
-                            zIndex: oAlert.baseZIndex + 1,
-                            display: 'none'
-                        })
-                        .click(function () {
-                            //Makes sure the alert is focused so the escape-key handler works
-                            oAlert.oButtonEl.focus();
-                        })
-                        .keydown(function (p_oEvent) {
-                            if (p_oEvent.keyCode === 27) {
-                                oAlert.hide();
-                            }
-                        })
-                        .append(this.oMessageEl)
-                        .append(this.oButtonContainerEl)
-                        .appendTo('body');
-
-                    /**
-                     * @private
-                     * @type {jQuery}
-                     */
-                    this.oBackgroundEl = jQuery('<div id="alerttoaster-background" />')
-                        .css({
-                            position: 'absolute',
-                            left: 0,
-                            top: 0,
-                            zIndex: oAlert.baseZIndex,
-                            display: 'none',
-                            width: '100%',
-                            height: '100%'
-                        })
-                        .click(function () {
-                            //Makes sure the alert is focused so the escape-key handler works
-                            oAlert.oButtonEl.focus();
-                        })
-                        .appendTo('body');
-
-                    /**
-                     * Returns the alert's jQuery
-                     * 
-                     * @return jQuery
-                     */
-                    this.getEl = function () {
-                        return this.oAlertEl;
-                    };
-
-                    /**
-                     * Sets the message
-                     * 
-                     * @private
-                     * @param {String} p_message
-                     */
-                    this.setMessage = function (p_message) {
-                        this.oMessageEl.html(p_message);
-                    };
-
-                    /**
-                     * Displays the specified message
-                     * 
-                     * @param {String} p_message
-                     */
-                    this.show = function (p_message) {
-                        var oAlert = this;
-
-                        this.setMessage(p_message);
-
-                        this.oBackgroundEl.fadeIn('fast', function () {
-                            oAlert.getEl().slideDown('fast');
-                            oAlert.oButtonEl.focus();
-                        });
-                    };
-
-                    /**
-                     * Hides the message
-                     */
-                    this.hide = function () {
-                        var oAlert = this;
-
-                        this.getEl().slideUp('fast', function () {
-                            oAlert.setMessage('');
-                            oAlert.oBackgroundEl.fadeOut('fast');
-                        });
-                    };
-
-                    return this;
-                }());
-
-            jQuery(window).resize(function () {
-                oAlertToaster.positionAlert();
-            });
-
-            return oAlert;
+        /**
+         * Horizontally-centres the alert in the window
+         * 
+         * @private
+         */
+        positionAlert: function () {
+            this.getAlert().setPosition(
+                parseInt((jQuery(window).width() / 2) - (this.getAlert().getDimensions().width / 2), 10) + 'px',
+                0
+            );
         },
 
         /**
@@ -184,21 +249,17 @@ jQuery.extend(true, {
          * @return Alert
          */
         getAlert: function () {
+            var oAlertToaster = this;
+
             if (this._getAlert() === null) {
                 this.setAlert(this.createAlert());
+
+                jQuery(window).resize(function () {
+                    oAlertToaster.positionAlert();
+                });
             }
 
             return this._getAlert();
-        },
-
-        /**
-         * Horizontally-centres the alert in the window
-         * 
-         * @private
-         */
-        positionAlert: function () {
-            var oAlertEl = this.getAlert().getEl();
-            oAlertEl.css('left', ((jQuery(window).width() / 2) - (oAlertEl.width() / 2)) + 'px');
         },
 
         /**
@@ -208,9 +269,8 @@ jQuery.extend(true, {
          * @todo Block until the message can be displayed?
          */
         showAlert: function (p_message) {
-            var oAlert = this.getAlert();
             this.positionAlert();
-            oAlert.show(p_message);
+            this.getAlert().show(p_message);
         },
 
         /**
